f33dd5c2cc058813d138d19fce830dd78c91daa4
rakyavaraartomily <rakyavara.artomily@sigma.co.id>
2024-02-06 08:57:00 +0700

    Initialize project using Create React App

---
+++ src/elements/Button/index.test.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Button from "./index";
+import { BrowserRouter as Router } from "react-router-dom";
+
+test("Should not allowed click button if isDisabled is present", () => {
+  const { container } = render(<Button isDisabled></Button>);
+
+  expect(container.querySelector("span.disabled")).toBeInTheDocument();
+});
+
+test("Should render loading/spinner", () => {
+  const { container, getByText } = render(<Button isLoading></Button>);
+
+  expect(getByText(/loading/i)).toBeInTheDocument();
+  expect(container.querySelector("span")).toBeInTheDocument();
+});
+
+test("Should render <a> tag", () => {
+  const { container } = render(
+    <Router>
+      <Button type="Link" isExternal></Button>
+    </Router>
+  );
+
+  expect(container.querySelector("a")).toBeInTheDocument();
+});
+
+test("Should render <link> component", () => {
+  const { container } = render(
+    <Router>
+      <Button type="Link"></Button>
+    </Router>
+  );
+
+  expect(container.querySelector("a")).toBeInTheDocument();
+});
